92843b0f12b03b1381e9dbce45dd228cb2ab8297
olliekennedy <ollie.kennedy1@gmail.com>
2020-10-13 17:13:50 +0100

    challenge 11 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,26 @@
 require './lib/bike.rb'
 
 class DockingStation
-   def release_bike
-     bike = Bike.new
-   end
+  def initialize
+    @bikes_docked = 1
+    @capacity = 4
+  end
+
+  def release_bike
+    if @bikes_docked > 0
+      @bikes_docked -= 1
+      bike = Bike.new
+    else
+      "There are no bikes"
+    end
+  end
+
+  def dock(bike)
+    if @bikes_docked < @capacity
+      @bikes_docked += 1
+      "Bike docked!"
+    else
+      "Find another docking station!"
+    end
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,3 +12,15 @@ describe DockingStation do
     expect(bike.working?).to eq true
   end
 end
+
+describe DockingStation do
+  it { is_expected.to respond_to(:dock).with(1).argument }
+end
+
+describe DockingStation do
+  it "feature test" do
+    station = DockingStation.new
+    bike = Bike.new
+    expect(station.dock(bike)).to eq "Bike docked!"
+  end
+end
